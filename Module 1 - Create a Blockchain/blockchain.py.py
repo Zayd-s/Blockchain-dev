29587a02943705113ed225b19ab06800d48eb9f5
Zayd S <zaydsabeh@hotmail.com>
2023-02-19 20:07:39 +0000

    Create Flask web app for http requests GET mine_block, get_chain & validity

--- Module 1 - Create a Blockchain/blockchain.py.py
+++ Module 1 - Create a Blockchain/blockchain.py.py
@@ -58,11 +58,48 @@ class Blockchain:
     
 # Mining our Blockchain
 
+# Web App
+app = Flask(__name__)
+#app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+blockchain = Blockchain()
 
-            
-        
-        
+# Mining a new block
+@app.route("/mine_block", methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'You just mined a block',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+#Get full Blockchain
+@app.route("/get_chain", methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+#Check validity of the blockchain
+@app.route('/valid', methods=['GET'])
+def valid():
+    valid = blockchain.is_chain_valid(blockchain.chain)
+ 
+    if valid:
+        response = {'message': 'The Blockchain is valid.'}
+    else:
+        response = {'message': 'The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+#Running the app
+app.run(host='0.0.0.0', port=5000)
+
         
         
         
